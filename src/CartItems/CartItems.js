1a05513332e4aa33af77095081946d9562bdf55e
Juan Baltazar <juan.baltazar1@gmail.com>
2019-10-26 16:43:11 -0700

    broke down cartitem component into cartchoices component

--- src/CartItems/CartItems.js
+++ src/CartItems/CartItems.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import CartChoices from '../Cartchoices/CartChoices';
 
 // This object will allow us to
 // easily convert numbers into US dollar values
@@ -14,24 +15,14 @@ class CartItems extends Component {
       (acc, curr) => acc + this.props.state.selected[curr].cost,
       0
     );
-    const summary = Object.keys(this.props.state.selected).map((feature, idx) => {
-      const featureHash = feature + '-' + idx;
-      const selectedOption = this.props.state.selected[feature];
-
-      return (
-        <div className="summary__option" key={featureHash}>
-          <div className="summary__option__label">{feature} </div>
-          <div className="summary__option__value">{selectedOption.name}</div>
-          <div className="summary__option__cost">
-            {USCurrencyFormat.format(selectedOption.cost)}
-          </div>
-        </div>
-      );
-    });
+    
     return (
     <>
       <h2>Your cart</h2>
-        {summary}
+        <CartChoices
+          state={this.props.state}
+          USCurrencyFormat={USCurrencyFormat}
+        />
       <div className="summary__total">
         <div className="summary__total__label">Total</div>
         <div className="summary__total__value">
